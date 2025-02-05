5b11b108cd537fc98ec0b9917c38e48a330b7c0d
Chinmay Mangalwedhe <cmangalwedhe@gmail.com>
2025-02-05 12:30:00 -0600

    added EMA and standard error averaging prior to LSTM to see efficacy

--- website/data_generator.py
+++ website/data_generator.py
@@ -1,99 +1,124 @@
-import numpy as np
-import os
-import urllib.request, json
-import datetime as dt
-import pandas as pd
 from pandas_datareader import data
+import matplotlib
+
+matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
-import tensorflow as tf
+import pandas as pd
+import datetime as dt
+import urllib.request, json
+import os
 import numpy as np
+import tensorflow as tf
 from sklearn.preprocessing import MinMaxScaler
+import yfinance as yf
 
+ticker = "AAL"
+file_to_save = f"stock_market_data_{ticker}_yf.csv"
 
-API_KEY = "I57LV5Y7UJALBAFS"
-ticker = "AAPL"
-url_string = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={ticker}&outputsize=full&apikey={API_KEY}"
+df = yf.download(ticker)
+df.to_csv("stock_market_data_1.csv")
+df = df.iloc[::-1]
+df2 = pd.DataFrame(columns=['Date', 'Low', 'High', 'Close', 'Open'])
 
-file_to_save = f"stock_market_data_{ticker}.csv"
+for index, row in df.iterrows():
+    date = str(index)[:str(index).find(" ")]
+    date_info = dt.datetime.strptime(date, "%Y-%m-%d")
+    df2.loc[-1, :] = [date_info.date(), row.iloc[2], row.iloc[1], row.iloc[0], row.iloc[3]]
+    df2.index += 1
 
-if not os.path.exists(file_to_save):
-    with urllib.request.urlopen(url_string) as url:
-        data = json.loads(url.read().decode())
-        data = data['Time Series (Daily)']
-        df = pd.DataFrame(columns=['Date', 'Low', 'High', 'Close', 'Open'])
+df2.to_csv(file_to_save)
 
-        for k, v in data.items():
-            date = dt.datetime.strptime(k, "%Y-%m-%d")
-            data_row = [date.date(), float(v['3. low']), float(v['2. high']), float(v['4. close']), float(v['1. open'])]
-            df.loc[-1,:] = data_row
-            df.index += 1
+plt.figure(figsize=(18,9))
+plt.plot(range(df2.shape[0]), (df2['Low']+df2['High'])/2.0)
+plt.xticks(range(0, df2.shape[0], 500), df2['Date'].loc[::500], rotation=45)
+plt.xlabel('Date', fontsize=18)
+plt.ylabel('Mid Price', fontsize=18)
+plt.show()
 
-        print(f"Data saved to: {file_to_save}")
-        df.to_csv(file_to_save)
-else:
-    print("File already exists. Loading data from CSV.")
-
-
-class DataGeneratorSeq:
-    def __init__(self, prices, batch_size, num_unroll):
-        self.prices = prices
-        self.prices_length = len(self.prices) - num_unroll
-        self.batch_size = batch_size
-        self.num_unroll = num_unroll
-        self.segments = self.prices_length // self.batch_size
-        self.cursor = [offset * self.segments for offset in range(self.batch_size)]
-
-    def next_batch(self):
-        batch_data = np.zeros(self.batch_size, dtype=np.float32)
-        batch_labels = np.zeros(self.batch_size, dtype=np.float32)
-
-        for batch in range(self.batch_size):
-            if self.cursor[batch] + 1 >= self.prices_length:
-                self.cursor[batch] = np.random.randint(0, (batch+1) * self.segments)
-
-            batch_data[batch] = self.prices[self.cursor[batch]]
-            batch_labels[batch] = self.prices[self.cursor[batch] + np.random.randint(0, 5)]
-
-            self.cursor[batch] = (self.cursor[batch] + 1) % self.prices_length
-
-        return batch_data, batch_labels
-
-    def unroll_batches(self):
-        unroll_data, unroll_labels = [], []
-        initial_data, initial_label = None, None
-
-        for ui in range(self.num_unroll):
-            data, labels = self.next_batch()
-            unroll_data.append(data)
-            unroll_labels.append(labels)
-
-        return unroll_data, unroll_labels
-
-    def reset_indices(self):
-        for b in range(self.batch_size):
-            self.cursor[b] = np.random.randint(0, min((b+1)*self.segments, self.prices_length-1))
-
-
-high_prices = df.loc[:,'High'].as_matrix()
-low_prices = df.loc[:,'Low'].as_matrix()
+high_prices = df.loc[:, 'High'].values
+low_prices = df.loc[:, 'Low'].values
 mid_prices = (high_prices + low_prices) / 2.0
 
-train_data = mid_prices[:11000]
-test_data = mid_prices[11000:]
+train_data = mid_prices[:730]
+test_data = mid_prices[730:]
 
 scaler = MinMaxScaler()
 train_data = train_data.reshape(-1, 1)
 test_data = test_data.reshape(-1, 1)
 
-smoothing_data_size = 2500
+smoothing_window_size = 20
 
-for di in range(0, 1000, smoothing_data_size):
-    scaler.fit(train_data[di:di+smoothing_data_size,:])
-    train_data[di:di+smoothing_data_size,:] = scaler.transform(train_data[di:di+smoothing_data_size,:])
+for di in range(0, 710, smoothing_window_size):
+    scaler.fit(train_data[di:di + smoothing_window_size, :])
+    train_data[di:di + smoothing_window_size, :] = scaler.transform(train_data[di:di + smoothing_window_size, :])
 
-scaler.fit(train_data[di + smoothing_data_size:,:])
-train_data[di+smoothing_data_size:,:] = scaler.transform(train_data)
+scaler.fit(train_data[di + smoothing_window_size:, :])
+train_data[di + smoothing_window_size:, :] = scaler.transform(train_data[di + smoothing_window_size:, :])
 
 train_data = train_data.reshape(-1)
 test_data = scaler.transform(test_data).reshape(-1)
 
+EMA = 0.0
+gamma = 0.1
+
+for ti in range(730):
+    EMA = gamma * train_data[ti] + (1 - gamma) * EMA
+    train_data[ti] = EMA
+
+all_mid_data = np.concatenate([train_data, test_data], axis=0)
+
+window_size = 100
+N = train_data.size
+std_avg_predictions = []
+std_avg_x = []
+mse_errors = []
+
+for pred_idx in range(window_size, N):
+    if pred_idx >= N:
+        date = dt.datetime.strptime(k, '%Y-%m-%d').date() + dt.timedelta(days=1)
+    else:
+        date = df2.loc[pred_idx, 'Date']
+
+    std_avg_predictions.append(np.mean(train_data[pred_idx - window_size:pred_idx]))
+    mse_errors.append((std_avg_predictions[-1] - train_data[pred_idx] ** 2))
+    std_avg_x.append(date)
+
+print(f"MSE error for standard averaging: {0.5 * np.mean(mse_errors)}")
+print(len(std_avg_predictions))
+
+"""plt.figure(figsize = (18,9))
+plt.plot(range(df.shape[0]), all_mid_data, color='b', label='True')
+plt.plot(range(window_size, N), std_avg_predictions, color='orange', label='Prediction')
+plt.xlabel('Date')
+plt.ylabel('Mid Price')
+plt.legend(fontsize=18)
+plt.show()"""
+
+window_size = 100
+N = train_data.size
+
+run_avg_predictions = []
+run_avg_x = []
+
+mse_errors = []
+
+running_mean = 0.0
+run_avg_predictions.append(running_mean)
+
+decay = 0.5
+
+for pred_idx in range(1, N):
+    running_mean = running_mean * decay + (1.0 - decay) * train_data[pred_idx - 1]
+    run_avg_predictions.append(running_mean)
+    mse_errors.append((run_avg_predictions[-1] - train_data[pred_idx]) ** 2)
+    run_avg_x.append(date)
+
+print(f'MSE error for EMA averaging: {0.5 * np.mean(mse_errors)}')
+
+plt.figure(figsize=(18,9))
+plt.plot(range(df2.shape[0]), all_mid_data, color='b', label='True')
+plt.plot(range(0,N), run_avg_predictions, color='orange', label='Prediction')
+plt.xlabel("Date")
+plt.ylabel("Prices")
+plt.legend(fontsize=18)
+plt.show()
